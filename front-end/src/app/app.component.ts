cea9005c694186e1b8329dfad0eb40d73571f240
TranDue <tran.congdue1999@gmail.con>
2020-12-01 14:49:12 +0700

    add project

---
+++ front-end/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Booke Here';
+  searchshow = false;
+  constructor() {
+    window.addEventListener("click", () => {
+      this.searchshow == true;
+    });
+  }
+  showSearchBox(): void {
+      console.log(this.searchshow);
+    this.searchshow = !this.searchshow;
+
+  }
+  onClick(event: Event): void {
+    event.stopPropagation();
+    this.searchshow = !this.searchshow;
+  }
+
+}
