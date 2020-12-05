a4db17800d595390efc734bf47e27545b463976d
TranDue <tran.congdue1999@gmail.con>
2020-12-05 14:24:02 +0700

    merge

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { WithlistComponent } from './header/withlist/withlist.component';
 const routes: Routes = [
-  { path:'', redirectTo:'',pathMatch:'full'},
-  { path: 'withlist', component:WithlistComponent},
 
 ];
 
